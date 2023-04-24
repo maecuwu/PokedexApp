6a5260428f6fb2bd542f090390eda82d085133a1
Imagar <java@imagar.com>
2023-04-24 11:02:31 +0200

    Stack navigator parte equipos

--- src/navigator/TeamsStackNavigator.tsx
+++ src/navigator/TeamsStackNavigator.tsx
@@ -2,12 +2,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { TeamScreen } from '../screens/TeamScreen';
 import { TeamsScreen } from '../screens/TeamsScreen';
 import { AddPokemonScreen } from '../screens/AddPokemonScreen';
+import { PokemonScreen } from '../screens/PokemonScreen';
+import { SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 
 export type TeamsRootStackParams = {
     TeamsScreen: undefined;
     TeamScreen: undefined;
     AddPokemonScreen: undefined;
+    PokemonScreen: { pokemon: SimplePokemon, bgColor: string, fontColor: string, addPossible: boolean};
 }
 
 
@@ -26,6 +29,7 @@ export const TeamsStackNavigator = () => {
             <Stack.Screen name="TeamsScreen" component={TeamsScreen} />
             <Stack.Screen name="TeamScreen" component={TeamScreen} />
             <Stack.Screen name="AddPokemonScreen" component={AddPokemonScreen} />
+            <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
         </Stack.Navigator>
     );
 }
--- src/screens/AddPokemonScreen.tsx
+++ src/screens/AddPokemonScreen.tsx
@@ -1,4 +1,5 @@
-import { Text, View, FlatList, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text, View, FlatList, Dimensions, TouchableOpacity } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { SearchInput } from '../components/SearchInput';
 import { globalStyles } from '../theme/appTheme';
@@ -7,12 +8,17 @@ import { PokemonCard } from '../components/PokemonCard';
 import { LoadingComponent } from '../components/LoadingComponent';
 import { useState, useEffect } from 'react';
 import { SimplePokemon } from '../interfaces/pokemonInterfaces';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { TeamsRootStackParams } from '../navigator/TeamsStackNavigator';
+import { StackScreenProps } from '@react-navigation/stack';
 
 
 const { width: screenWidth } = Dimensions.get('window');
 
 
-export const AddPokemonScreen = () => {
+interface Props extends StackScreenProps<TeamsRootStackParams, 'AddPokemonScreen'> { };
+
+export const AddPokemonScreen = ({ navigation }: Props) => {
 
     const { top } = useSafeAreaInsets();
 
@@ -55,13 +61,23 @@ export const AddPokemonScreen = () => {
     return (
         <View style={{ flex: 1, marginHorizontal: 20 }}>
 
+            <TouchableOpacity
+                activeOpacity={0.7}
+                style={{ ...styles.backButton, top: top + 10 }}
+                onPress={() => navigation.goBack()}
+            >
+
+                <Icon name='arrow-back-outline' color='black' size={35} />
+
+            </TouchableOpacity>
+
             <SearchInput
                 onDebounce={(value) => setSearchString(value)}
                 style={{
                     position: 'absolute',
                     zIndex: 999,
                     width: screenWidth - 40,
-                    top: top + 10
+                    top: top + 60
                 }}
             />
 
@@ -74,7 +90,7 @@ export const AddPokemonScreen = () => {
                         color: 'black',
                         marginBottom: 20,
                         paddingBottom: 10,
-                        marginTop: top + 60
+                        marginTop: top + 80
                     }}>
                         {searchString}
                     </Text>
@@ -85,9 +101,18 @@ export const AddPokemonScreen = () => {
                 showsVerticalScrollIndicator={false}
                 numColumns={2}
                 renderItem={({ item }) => (
-                    <PokemonCard pokemon={item} addPossible/>
+                    <PokemonCard pokemon={item} addPossible />
                 )}
             />
         </View>
     )
 }
+
+
+
+export const styles = StyleSheet.create({
+    backButton: {
+        position: 'absolute',
+        zIndex: 999
+    },
+});
